ac5b1b0a693b1bffe0a2b9a4a38bb6692d33ebb2
Beatriz Lima <beatrizlima.developer@gmail.com>
2023-09-21 20:27:12 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX - Pesquisar sobre oque é
+    return (
+        <header className='banner'>
+            <img src="\imagens\banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
